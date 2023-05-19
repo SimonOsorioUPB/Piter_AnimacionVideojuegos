5f3c978071b97c30b39de7068bf1a848cded4fe3
Dr. 27 <soranokuroi@gmail.com>
2023-05-18 21:49:18 -0500

    Procedurales

--- Assets/_Osorio/Scripts/SeePoint.cs
+++ Assets/_Osorio/Scripts/SeePoint.cs
@@ -1,41 +1,51 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
-public class Osorio_IKSurface : MonoBehaviour
+public class SeePoint : MonoBehaviour
 {
     private const float REFRESH_DELAY = 0.5f;
-    
+    [SerializeField][Range(0, 1)] private float proceduralInfluence;
     [SerializeField] private float detectionRadius;
     [SerializeField] private LayerMask detectionMask;
     [SerializeField] private Transform referencePoint;
     [SerializeField] private Transform raycastReference;
-    
+
+    [SerializeField] private MultiAimConstraint[] multiAimConstraints;
+
+    [SerializeField] private AnimationCurve activationAnimation;
+    [SerializeField] private AnimationCurve deactivationAnimation;
     //Encontrar objetos idoneos
-    
+    public bool IsCollicion;
+    public bool currentOverride;
     /// <summary>
     /// Returns all suitable objects for IK Snap
     /// </summary>
+    /// 
     private Collider[] Query()
     {
         return Physics.OverlapSphere(referencePoint.position, detectionRadius, detectionMask);
     }
-    
-    //Encontrar punto de referencia m√°s cercano
+
+    //Encontrar punto de referencia m·s cercano
     private bool GetNearestPositionForSnap(Collider[] nearColliders, out Vector3 nearestPoint)
     {
         try
         {
             //Encontrar punto mas cercano de la superficie
-        
-            //Se crea una lista de los puntos m√°s cercanos a los colliders
+
+            //Se crea una lista de los puntos m·s cercanos a los colliders
             var closestPoints = nearColliders.Select(collider => collider.ClosestPoint(referencePoint.position));
-        
-            //Se encuentra el punto m√°s cercano, del collider m√°s cercano
+
+            //Se encuentra el punto m·s cercano, del collider m·s cercano
             Vector3 closestPoint = closestPoints.OrderBy(position => Vector3.Distance(referencePoint.position, position))
                 .First();
-            
-            //Evaluar si el punto de referencia est√° dentro del collider
+
+            //Evaluar si el punto de referencia est· dentro del collider
             if (closestPoint == referencePoint.position)
             {
                 //Estoy dentro
@@ -64,24 +74,61 @@ public class Osorio_IKSurface : MonoBehaviour
         nearestPoint = transform.position;
         return false;
     }
-
     private void LateUpdate()
     {
         if (GetNearestPositionForSnap(Query(), out Vector3 nearestPosition))
         {
-            //Superficie con posici√≥n valida cerca
+            //Superficie con posiciÛn valida cerca
             transform.position = nearestPosition;
-            //Mandar la se√±al
-            gameObject.SendMessage("OverrideIK", true, SendMessageOptions.DontRequireReceiver);
+            //Mandar la seÒal
+            Debug.Log("collider");
+            IsCollicion = true;
+
+            OverrideIK(true);
         }
         else
         {
-            gameObject.SendMessage("OverrideIK", false, SendMessageOptions.DontRequireReceiver);
+            Debug.Log("on collider");
+            IsCollicion = false;
+            OverrideIK(false);
+
+        }
+    }
+    public void OverrideIK(bool state)
+    {
+
+        if (state != currentOverride)
+        {
+            Debug.Log("cambio "+ state);
+            currentOverride = state;
+            StartCoroutine(AnimaeInfluence());
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(referencePoint.position, detectionRadius);
+    }
+    IEnumerator AnimaeInfluence()
+    {
+        //actualizacion las influencias
+
+        AnimationCurve curve = IsCollicion ?  deactivationAnimation: activationAnimation;
+        for (float time = 0; time < 1f; time += Time.deltaTime*1f)
+        {
+            proceduralInfluence = curve.Evaluate(time);
+            UpdateInfluence(proceduralInfluence);
+            yield return null;
+        }
+    }
+    private void UpdateInfluence(float weight)
+    {
+        if (multiAimConstraints == null) return;
+
+        foreach (MultiAimConstraint multiAimConstraint in multiAimConstraints)
+        {
+            if (multiAimConstraint == null) continue;
+            multiAimConstraint.weight = 1 - weight;
+        }
     }
 }
